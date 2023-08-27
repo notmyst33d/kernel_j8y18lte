1386d2bc333285a35e8550998d573148a967ffac
Myst33d <notmyst33d@gmail.com>
2023-08-27 21:21:32 +0600

    fms: Add "cdrom" node to sysfs

--- drivers/usb/gadget/function/f_mass_storage.c
+++ drivers/usb/gadget/function/f_mass_storage.c
@@ -3236,9 +3236,26 @@ static ssize_t file_store(struct device *dev, struct device_attribute *attr,
 	return fsg_store_file(curlun, filesem, buf, count);
 }
 
+static ssize_t cdrom_show(struct device *dev, struct device_attribute *attr, char *buf)
+{
+	struct fsg_lun		*curlun = fsg_lun_from_dev(dev);
+
+	return fsg_show_cdrom(curlun, buf);
+}
+
+static ssize_t cdrom_store(struct device *dev, struct device_attribute *attr,
+			const char *buf, size_t count)
+{
+	struct fsg_lun		*curlun = fsg_lun_from_dev(dev);
+	struct rw_semaphore	*filesem = dev_get_drvdata(dev);
+
+	return fsg_store_cdrom(curlun, filesem, buf, count);
+}
+
 static DEVICE_ATTR_RW(ro);
 static DEVICE_ATTR_RW(nofua);
 static DEVICE_ATTR_RW(file);
+static DEVICE_ATTR_RW(cdrom);
 static DEVICE_ATTR(perf, 0644, fsg_show_perf, fsg_store_perf);
 
 static struct device_attribute dev_attr_ro_cdrom = __ATTR_RO(ro);
@@ -3390,6 +3407,7 @@ static inline void fsg_common_remove_sysfs(struct fsg_lun *lun)
 	device_remove_file(&lun->dev, &dev_attr_ro);
 	device_remove_file(&lun->dev, &dev_attr_file);
 	device_remove_file(&lun->dev, &dev_attr_perf);
+    device_remove_file(&lun->dev, &dev_attr_cdrom);
 }
 
 void fsg_common_remove_lun(struct fsg_lun *lun, bool sysfs)
@@ -3511,6 +3529,10 @@ static inline int fsg_common_add_sysfs(struct fsg_common *common,
 		put_device(&lun->dev);
 		return rc;
 	}
+
+    rc = device_create_file(&lun->dev, &dev_attr_cdrom);
+    if (rc)
+        goto error;
 
 	rc = device_create_file(&lun->dev,
 				lun->cdrom
